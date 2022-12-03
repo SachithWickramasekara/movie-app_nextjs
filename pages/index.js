8af3ce5b71f9ff810d3ecc6f83aa662808631b48
SachithWickramasekara <sachithwickramasekara@gmail.com>
2022-12-04 01:02:21 +0530

    initialized the code

---
+++ pages/index.js
@@ -0,0 +1,21 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>HULU</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+    {/*Header*/}
+
+    {/* Nav */}
+
+    {/* Results */}
+
+    </div>
+  )
+}
